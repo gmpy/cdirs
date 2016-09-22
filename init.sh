7f43cd9468cb018948788150140bd583c638db4d
gmpy <gmpy_tiger@163.com>
2016-09-22 11:54:00 +0800

    cdir: fix bug that cdir with more than 1 parameter
    Signed-off-by: gmpy <gmpy_tiger@163.com>

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 #   the entry of cdir, setdir, lsdir, rmdir, whisch is to set for env functions
 
 cdir() {
+    if [ "$#" -gt "1" ]; then
+        echo "Usage: cdir <num|label|path>"
+        return 0
+    fi
     cd `scdir cdir $@`
 }
 
