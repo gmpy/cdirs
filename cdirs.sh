d35947bed09cb64c65020422a23a9aa15a289778
gmpy <gmpy_tiger@163.com>
2016-09-27 14:17:22 +0800

    fix bug: ensure cd is a builtin command before cd
    Signed-off-by: gmpy <gmpy_tiger@163.com>

--- cdirs.sh
+++ cdirs.sh
@@ -128,7 +128,7 @@ clear_global_dir() {
 replace_cd() {
     local alias_cd=$(alias | grep "cd=.*$" | awk '{print $2}')
     [ -n "${alias_cd}" ] && unalias cd
-    cd $1
+    [ "$(type -t cd)" = "builtin" ] && cd $1
     [ -n "${alias_cd}" ] && eval alias ${alias_cd}
 }
 
