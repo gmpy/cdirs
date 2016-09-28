0f1561d5c4cd303b35cb4e2a41a22e1d72ed5599
gmpy <gmpy_tiger@163.com>
2016-09-28 10:49:49 +0800

    add "" to all supported variable
    Signed-off-by: gmpy <gmpy_tiger@163.com>

--- cdirs.sh
+++ cdirs.sh
@@ -2,8 +2,8 @@
 
 cdir() {
     local force_type
-    local opts=$(getopt -l "num,label,path,help" -o "hnlp" -- $@) || return -1
-    eval set -- ${opts}
+    local opts="$(getopt -l "num,label,path,help" -o "hnlp" -- $@)" || return -1
+    eval set -- "${opts}"
     while true
     do
         case "$1" in
@@ -38,8 +38,8 @@ cdir() {
 }
 
 setdir() {
-    local opts=$(getopt -l "global,help" -o "hg" -- $@) || return -1
-    eval set -- ${opts}
+    local opts="$(getopt -l "global,help" -o "hg" -- $@)" || return -1
+    eval set -- "${opts}"
     while true
     do
         case "$1" in
@@ -47,7 +47,7 @@ setdir() {
                 shift
                 ;;
             -g|--global)
-                _setdir $(eval "echo \$$(( $# - 1 ))") $(eval "echo \$$#") global
+                _setdir "$(eval "echo \$$(( $# - 1 ))")" "$(eval "echo \$$#")" "global"
                 return 0
                 ;;
             --)
@@ -56,18 +56,16 @@ setdir() {
         esac
     done
 
-    if [ $# -ne 2 ]; then
+    if [ "$#" -ne "2" ]; then
         _setdir "$1" "$(shift;echo "$*")"
-#        echo -e "\033[31mUsage: setdir <label> <path>\033[0m"
-#        return -1
     else
         _setdir $@
     fi
 }
 
 lsdir() {
-    local opts=$(getopt -l "print:,help" -o "hp:" -- $@) || return -1
-    eval set -- ${opts}
+    local opts="$(getopt -l "print:,help" -o "hp:" -- $@)" || return -1
+    eval set -- "${opts}"
     while true
     do
         case "$1" in
@@ -76,7 +74,7 @@ lsdir() {
                 ;;
             -p|--print)
                 local path="$(get_path $2)"
-                echo ${path} | grep "^.*/$" &>/dev/null && echo ${path%/*} || echo ${path}
+                echo "${path}" | grep "^.*/$" &>/dev/null && echo "${path%/*}" || echo "${path}"
                 return 0
                 ;;
             --)
@@ -90,9 +88,9 @@ lsdir() {
 }
 
 cldir() {
-    local opts=$(getopt -l "all,reset,help,reload,global" -o "gha" -- $@) || return -1
+    local opts="$(getopt -l "all,reset,help,reload,global" -o "gha" -- $@)" || return -1
     local global_flag=0
-    eval set -- ${opts}
+    eval set -- "${opts}"
     while true
     do
         case "$1" in
@@ -127,9 +125,9 @@ cldir() {
     done
 
     if [ "${global_flag}" -eq "1" ]; then
-        for (( num=1; num<=${#}; num++ ))
+        for (( num=1; num<=$#; num++ ))
         do
-            clear_global_dir $(eval echo "\$${num}")
+            clear_global_dir "$(eval echo "\$${num}")"
         done
     fi
     _cldir $@
@@ -139,26 +137,26 @@ cldir() {
 clear_global_dir() {
     case "$(check_type $1)" in
         num)
-            clear_global_dir_from_label $(get_label_from_env $(get_env_from_num $1))
+            clear_global_dir_from_label "$(get_label_from_env "$(get_env_from_num "$1")")"
             ;;
         label)
-            clear_global_dir_from_label $1
+            clear_global_dir_from_label "$1"
             ;;
         path)
-            local path=$(get_absolute_path $1)
-            clear_global_dir_from_label $(get_label_from_env $(get_env_from_path ${path}))
+            local path="$(get_absolute_path "$1")"
+            clear_global_dir_from_label "$(get_label_from_env "$(get_env_from_path "${path}")")"
             ;;
     esac
 }
 
 # replace_cd <path>
 replace_cd() {
-    local alias_cd=$(alias | grep "cd=.*$" | awk '{print $2}')
+    local alias_cd="$(alias | grep "cd=.*$" | awk '{print $2}')"
     [ -n "${alias_cd}" ] && unalias cd
     if [ "$(type -t cd)" = "builtin" ]; then
         [ -n "$*" ] && cd "$*" || cd
     fi
-    [ -n "${alias_cd}" ] && eval alias ${alias_cd}
+    [ -n "${alias_cd}" ] && eval alias "${alias_cd}"
 }
 
 # reset
@@ -170,18 +168,21 @@ reset() {
 
 # clear_all
 clear_all() {
-    for env in $(get_all_env)
+    local oIFS="${IFS}"
+    IFS=$'\n'
+    for env in "$(get_all_env)"
     do
-        clear_dir_from_num $(get_num_from_env ${env})
+        clear_dir_from_num "$(get_num_from_env "${env}")"
     done
+    IFS="${IFS}"
 
     gmpy_cdir_initialized=0
     gmpy_cdir_cnt=0
 }
 
 gmpy_init() {
-    local opts=$(getopt -l "replace-cd,help" -o "h" -- $@) || return -1
-    eval set -- ${opts}
+    local opts="$(getopt -l "replace-cd,help" -o "h" -- $@)" || return -1
+    eval set -- "${opts}"
     while true
     do
         case "$1" in
@@ -213,33 +214,33 @@ gmpy_init() {
 # complete_func <input>
 complete_func() {
     local cmd="${1##*/}"
-    local word=${COMP_WORDS[COMP_CWORD]}
-    local line=${COMP_LINE}
+    local word="${COMP_WORDS[COMP_CWORD]}"
+    local line="${COMP_LINE}"
     local complete_list
     local opts_cnt
 
-    case ${cmd} in
+    case "${cmd}" in
         cldir|lsdir)
-            complete_list=$(get_all_label)
+            complete_list="$(get_all_label)"
             ;;
         setdir)
-            opts_cnt=$(echo ${line} | sed -r 's/[^-][[:alpha:]]+//g' | wc -w)
-            [ "$(( ${COMP_CWORD} - ${opts_cnt} ))" -eq "1" ] && complete_list=$(get_all_label)
+            opts_cnt="$(echo "${line}" | sed -r 's/[^-][[:alpha:]]+//g' | wc -w)"
+            [ "$(( ${COMP_CWORD} - ${opts_cnt} ))" -eq "1" ] && complete_list="$(get_all_label)"
             ;;
         cd|cdir)
-            case ${COMP_WORDS[$(( ${COMP_CWORD} - 1 ))]} in
+            case "${COMP_WORDS[$(( ${COMP_CWORD} - 1 ))]}" in
                 "-l"|"--label")
-                    complete_list=$(get_all_label)
+                    complete_list="$(get_all_label)"
                     ;;
                 "-n"|"--num")
-                    complete_list=$(get_all_num)
+                    complete_list="$(get_all_num)"
                     ;;
                 "-p"|"--path")
                     complete_list=
                     ;;
                 *)
-                    opts_cnt=$(echo ${line} | sed -r 's/[^-][[:alpha:]]+//g' | wc -w)
-                    [ "$(( ${COMP_CWORD} - ${opts_cnt} ))" -eq "1" ] && complete_list=$(get_all_label)
+                    opts_cnt="$(echo ${line} | sed -r 's/[^-][[:alpha:]]+//g' | wc -w)"
+                    [ "$(( ${COMP_CWORD} - ${opts_cnt} ))" -eq "1" ] && complete_list="$(get_all_label)"
                     ;;
             esac
             ;;
@@ -249,18 +250,24 @@ complete_func() {
 
 # get_all_num
 get_all_num() {
+    local oIFS="${IFS}"
+    IFS=$'\n'
     for env in $(get_all_env)
     do
         echo -n "$(get_num_from_env ${env}) "
     done
+    local IFS="${oIFS}"
 }
 
 # get_all_label
 get_all_label() {
+    local oIFS="${IFS}"
+    IFS=$'\n'
     for env in $(get_all_env)
     do
         echo -n "$(get_label_from_env ${env}) "
     done
+    local IFS="${oIFS}"
 }
 
 # load_default_label
@@ -269,10 +276,15 @@ load_default_label() {
     [ "${gmpy_cdir_initialized}" = "1" ] && return 0
     [ ! -f ~/.cdir_default ] && return -1
 
+    local oIFS="${IFS}"
+    IFS=$'\n'
     for line in $(cat ~/.cdir_default | egrep -v "^#.*$|^$" | grep "=")
     do
-         _setdir $(echo $line | sed "s/=/ /g") "no_print"
+        IFS="${oIFS}"
+         _setdir $(echo "$line" | sed "s/=/ /g") "no_print"
+        IFS=$'\n'
     done
+    IFS="${oIFS}"
     gmpy_cdir_initialized=1
 }
 
@@ -280,41 +292,41 @@ load_default_label() {
 # echo the result
 get_path() {
     local path
-    case "$([ -n "$2" ] && echo $2 || check_type "$1")" in
+    case "$([ -n "$2" ] && echo "$2" || check_type "$1")" in
         "path")
             path="$1"
             ;;
         "num")
-            path=$(get_path_from_num $1)
+            path="$(get_path_from_num "$1")"
             ;;
         "label")
-            [ "$(check_label $1)" = "yes" ] && path=$(get_path_from_label $1)
+            [ "$(check_label $1)" = "yes" ] && path="$(get_path_from_label "$1")"
             ;;
     esac
 
-    [ -n "${path}" ] && echo ${path} || echo $1
+    [ -n "${path}" ] && echo "${path}" || echo "$1"
 }
 
 # check_label <label>
 check_label() {
-    [ -n "$(echo $1 | egrep "^[[:alpha:]]([[:alnum:]]*_*[[:alnum:]]*)*$")" ] && echo yes || echo no
+    [ -n "$(echo "$1" | egrep "^[[:alpha:]]([[:alnum:]]*_*[[:alnum:]]*)*$")" ] && echo yes || echo no
 }
 
 # get_path_from_num <num>
 get_path_from_num() {
-    local env=$(get_env_from_num "$1" | head -n 1)
-    [ -n "${env}" ] && echo $(get_path_from_env "${env}")
+    local env="$(get_env_from_num "$1" | head -n 1)"
+    [ -n "${env}" ] && echo "$(get_path_from_env "${env}")"
 }
 
 # get_path_from_label <label>
 get_path_from_label() {
-    local env=$(get_env_from_label "$1" | head -n 1)
-    [ -n "${env}" ] && echo $(get_path_from_env "${env}") || echo $1
+    local env="$(get_env_from_label "$1" | head -n 1)"
+    [ -n "${env}" ] && echo "$(get_path_from_env "${env}")" || echo "$1"
 }
 
 # is_exited_dir <label|num|path>
 is_exited_dir() {
-    case ${1} in
+    case "${1}" in
         -)
             echo yes
             ;;
@@ -330,25 +342,25 @@ is_exited_dir() {
 # path: string wiht ./ or ../ or /, sometime it can be ~ or begin with ~
 # label: other else
 check_type() {
-    if [ $# -ne 1 ]; then
+    if [ "$#" -ne "1" ]; then
         return -1
     fi
 
-    if $(echo $1 | egrep "^[0-9]+$" &>/dev/null); then
-        echo num
+    if $(echo "$1" | egrep "^[0-9]+$" &>/dev/null); then
+        echo "num"
         return 0
     fi
 
-    if [ -n "$(echo $1 | egrep "\./|\.\./|/")" ] \
+    if [ -n "$(echo "$1" | egrep "\./|\.\./|/")" ] \
         || [ "${1:0:1}" = "~" ] \
         || [ "$1" = "-" ] \
         || [ "$1" = "." ] \
         || [ "$1" = ".." ]; then
-        echo path
+        echo "path"
         return 0
     fi
 
-    echo label
+    echo "label"
 }
 
 # set_env <var> <path>
@@ -380,10 +392,10 @@ get_absolute_path() {
 
 
     # deal with word like ./ ../
-    while [ -n "$(echo ${para_path} | egrep "\./|\.\./")" ]
+    while [ -n "$(echo "${para_path}" | egrep "\./|\.\./")" ]
     do
         if [ "${para_path%%/*}" = ".." ]; then
-            pwd_path=${pwd_path%/*}
+            pwd_path="${pwd_path%/*}"
         elif [ ! "${para_path%%/*}" = "." ]; then
             pwd_path="${pwd_path}/${para_path%%/*}"
         fi
@@ -395,7 +407,7 @@ get_absolute_path() {
     elif [ -d "${para_path}" ] && [ ! "${para_path:0:1}" = "/" ]; then
         echo "${PWD}/${para_path}"
     else
-        echo ${para_path}
+        echo "${para_path}"
     fi
 }
 
@@ -403,14 +415,14 @@ get_absolute_path() {
 # $3 can be that no_print,global
 _setdir() {
     #get path
-    local path=$(get_absolute_path "$2")
+    local path="$(get_absolute_path "$2")"
 
     if [ "$(is_exited_dir "${path}")" = "no" ]; then
         echo -e "\033[31m${path} is not existed\033[0m"
         return -1
     fi
 
-    if [ "$(check_label $1)" = "no" ];then
+    if [ "$(check_label "$1")" = "no" ];then
         echo -en "\033[31mlabel error: \033[0m"
         echo "label start with a letter and is a combination of letters, numbers and _"
         return -1
@@ -418,14 +430,14 @@ _setdir() {
 
 
     #get var
-    local var=$(get_env_from_label $1 | head -n 1)
+    local var="$(get_env_from_label $1 | head -n 1)"
     if [ -n "${var}" ]; then
-        echo $3 | grep -w "no_print" &>/dev/null || echo -en "\033[31mmodify:\033[0m\t"
-        var=${var%%=*}
+        echo "$3" | grep -w "no_print" &>/dev/null || echo -en "\033[31mmodify:\033[0m\t"
+        var="${var%%=*}"
     else
-        echo $3 | grep -w "no_print" &>/dev/null || echo -en "\033[31mcreate:\033[0m\t"
+        echo "$3" | grep -w "no_print" &>/dev/null || echo -en "\033[31mcreate:\033[0m\t"
         add_num_cnt
-        var=${gmpy_cdir_prefix}_$(get_num_cnt)_$1
+        var="${gmpy_cdir_prefix}_$(get_num_cnt)_$1"
     fi
 
     if [ -n "${path}" ] && [ -n "${var}" ]; then
@@ -434,7 +446,7 @@ _setdir() {
             clear_global_dir_from_label "$1"
             set_dir_defalut "$1" "${path}"
         fi
-        echo "$3" | grep -w "no_print" &>/dev/null || ls_format "$(get_env_from_label $1 | head -n 1)"
+        echo "$3" | grep -w "no_print" &>/dev/null || ls_format "$(get_env_from_label "$1" | head -n 1)"
     fi
 }
 
@@ -444,7 +456,7 @@ clear_global_dir_from_label() {
     local label
     for (( num=1; num<=$# ; num++ ))
     do
-        label=$(eval echo \$${num})
+        label="$(eval echo \$${num})"
         sed -i "/^${label}=.*$/d" ~/.cdir_default
     done
 }
@@ -457,23 +469,23 @@ set_dir_defalut() {
 # _cdir <label|num|path> [num|label|path](point out the type)
 _cdir() {
     if [ -n "$2" ]; then
-        echo $(get_path "$1" "$2")
+        echo "$(get_path "$1" "$2")"
     else
         if [ "`is_exited_dir "$1"`" = "yes" ]; then
-            echo $1
+            echo "$1"
             return 0
         fi
 
-        echo $(get_path "$1")
+        echo "$(get_path "$1")"
     fi
 }
 
 # _lsdir [num1|label1|path1] [num2|label2|path2] ...
 _lsdir() {
-    if [ $# -gt 0 ]; then
+    if [ "$#" -gt 0 ]; then
         for para in $@
         do
-            ls_one_dir $para
+            ls_one_dir "${para}"
         done
     else
         ls_all_dirs
@@ -482,7 +494,7 @@ _lsdir() {
 
 # ls_all_dirs
 ls_all_dirs() {
-    local oIFS=${IFS}
+    local oIFS="${IFS}"
     IFS=$'\n'
     for env in $(get_all_env)
     do
@@ -498,36 +510,36 @@ get_all_env() {
 
 get_num_cnt() {
     [ -z "${gmpy_cdir_cnt}" ] && export gmpy_cdir_cnt=0
-    echo ${gmpy_cdir_cnt}
+    echo "${gmpy_cdir_cnt}"
 }
 
 add_num_cnt() {
-    export gmpy_cdir_cnt=$(( $(get_num_cnt) + 1 ))
+    export gmpy_cdir_cnt="$(( $(get_num_cnt) + 1 ))"
 }
 
 # ls_one_dir <num|label|path>
 ls_one_dir() {
-    case "`check_type $1`" in
+    case "`check_type "$1"`" in
         num)
-            ls_format "$(get_env_from_num $1)"
+            ls_format "$(get_env_from_num "$1")"
             ;;
         path)
-            local oIFS=${IFS}
+            local oIFS="${IFS}"
             IFS=$'\n'
             for env in $(get_env_from_path "$(get_absolute_path "$1")")
             do
-                ls_format ${env}
+                ls_format "${env}"
             done
-            IFS=${oIFS}
+            IFS="${oIFS}"
             ;;
         label)  #support regular expression
-            local oIFS=${IFS}
+            local oIFS="${IFS}"
             IFS=$'\n'
-            for env in $(get_env_from_label $1)
+            for env in $(get_env_from_label "$1")
             do
                 ls_format "${env}"
             done
-            IFS=${oIFS}
+            IFS="${oIFS}"
             ;;
     esac
 }
@@ -603,55 +615,55 @@ _cldir() {
 
 # get_var_from_env <env>
 get_var_from_env() {
-   echo ${1%=*} 
+   echo "${1%=*}"
 }
 
 # clear_dir_from_num <num>
 clear_dir_from_num() {
-    local env=$(get_env_from_num $1)
-    unset $(get_var_from_env ${env}) && echo -e "\033[31mdelete:\033[0m\t$(ls_format ${env})"
+    local env="$(get_env_from_num "$1")"
+    unset "$(get_var_from_env "${env}")" && echo -e "\033[31mdelete:\033[0m\t$(ls_format "${env}")"
 }
 
 # clear_dir_from_path <path>
 clear_dir_from_path() {
     local oIFS=${IFS}
     IFS=$'\n'
-    for env in $(get_env_from_path $(get_absolute_path $1))
+    for env in $(get_env_from_path "$(get_absolute_path "$1")")
     do
-        unset $(get_var_from_env ${env}) && echo -e "\033[31mdelete:\033[0m\t$(ls_format ${env})"
+        unset "$(get_var_from_env "${env}")" && echo -e "\033[31mdelete:\033[0m\t$(ls_format "${env}")"
     done
-    IFS=${oIFS}
+    IFS="${oIFS}"
 }
 
 # clear_dir_from_label <label>
 clear_dir_from_label() {
     local oIFS="${IFS}"
     IFS=$'\n'
-    for env in $(get_env_from_label $1)
+    for env in $(get_env_from_label "$1")
     do
-        unset $(get_var_from_env ${env}) && echo -e "\033[31mdelete:\033[0m\t$(ls_format ${env})"
+        unset "$(get_var_from_env "${env}")" && echo -e "\033[31mdelete:\033[0m\t$(ls_format "${env}")"
     done
     IFS="${oIFS}"
 }
 
 # get_env_from_num <num>
 get_env_from_num() {
-    local env=$(env | grep "^${gmpy_cdir_prefix}_$1_.*=.*$")
-    [ $(echo "${env}" | wc -l) -eq 1 ] && echo "${env}"
+    local env="$(env | grep "^${gmpy_cdir_prefix}_$1_.*=.*$")"
+    [ "$(echo "${env}" | wc -l)" -eq "1" ] && echo "${env}"
 }
 
 # get_env_from_path <path>
 # enable echo more than one env
 get_env_from_path() {
-    local env=$(env | egrep "^${gmpy_cdir_prefix}_[0-9]+_.*=$1/?$" | sort)
+    local env="$(env | egrep "^${gmpy_cdir_prefix}_[0-9]+_.*=$1/?$" | sort)"
     [ -n "${env}" ] && echo "${env}"
 }
 
 # get_env_from_label <label>
 # enable echo more than one env if input regular expression
 get_env_from_label() {
-    local env=$(env | egrep "^${gmpy_cdir_prefix}_[0-9]+_$1=.*$" | sort)
-    [ $(echo "${env}" | wc -l) -eq 1 ] && echo "${env}"
+    local env="$(env | egrep "^${gmpy_cdir_prefix}_[0-9]+_$1=.*$" | sort)"
+    [ "$(echo "${env}" | wc -l)" -eq "1" ] && echo "${env}"
 }
 
 gmpy_init $@
